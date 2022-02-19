5f1938e470a55212c281fb8903257df139790e4e
Cynthia France <cf219@duke.edu>
2022-02-19 04:05:11 -0500

    connected turtle nad insn models and wrote basic structure to call methods (untested as of rn)

--- src/main/java/slogo/Model/TurtleModel.java
+++ src/main/java/slogo/Model/TurtleModel.java
@@ -1,7 +1,46 @@
 package slogo.Model;
 
-public class TurtleModel {
-  public TurtleModel() {
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
+public class TurtleModel {
+  InstructionModel insnModel;
+  double x;
+  double y;
+  //assumption: facing right = 0 degrees, increases clockwise
+  int heading;
+
+  public TurtleModel(double startX, double startY, int startHeading) {
+    insnModel = new InstructionModel();
+    x = startX;
+    y = startY;
+    heading = startHeading;
+  }
+
+  public Object runNextInsn()
+      throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    String[] insn = insnModel.getNextInsn().split(" ");
+    Method m = this.getClass().getDeclaredMethod(insn[0]);
+    return m.invoke(this, Arrays.copyOfRange(insn, 1, insn.length));
+  }
+
+  public void addInsn(String instruction) {
+    insnModel.addInsn(instruction);
+  }
+
+  private int forward(int pixels) {
+    x = x + calcXchange(pixels);
+    y = y + calcYchange(pixels);
+    return pixels;
+  }
+
+  private double calcXchange(int pixels) {
+    return pixels * Math.cos(heading);
+  }
+
+  private double calcYchange(int pixels) {
+    return pixels * Math.sin(heading);
   }
 }
