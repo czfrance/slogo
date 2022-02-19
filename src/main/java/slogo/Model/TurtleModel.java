73b47937624852a913cab92df178a7c77d0283e2
Cynthia France <cf219@duke.edu>
2022-02-19 16:14:34 -0500

    used Reflections to implement first method (forward)

--- src/main/java/slogo/Model/TurtleModel.java
+++ src/main/java/slogo/Model/TurtleModel.java
@@ -2,6 +2,7 @@ package slogo.Model;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,25 +23,44 @@ public class TurtleModel {
   public Object runNextInsn()
       throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
     String[] insn = insnModel.getNextInsn().split(" ");
-    Method m = this.getClass().getDeclaredMethod(insn[0]);
-    return m.invoke(this, Arrays.copyOfRange(insn, 1, insn.length));
+    int[] params = makeInts(insn);
+    Method m = this.getClass().getDeclaredMethod(insn[0], params.getClass());
+    return m.invoke(this, params);
   }
 
   public void addInsn(String instruction) {
     insnModel.addInsn(instruction);
   }
 
-  private int forward(int pixels) {
+  private int[] makeInts(String[] insn) {
+    int[] params = new int[insn.length-1];
+    for (int i = 1; i < insn.length; i++) {
+      params[i-1] = Integer.parseInt(insn[i]);
+    }
+    return params;
+  }
+
+  private int forward(int[] params) {
+    System.out.println("forward");
+    int pixels = params[0];
     x = x + calcXchange(pixels);
     y = y + calcYchange(pixels);
     return pixels;
   }
 
   private double calcXchange(int pixels) {
-    return pixels * Math.cos(heading);
+    return pixels * Math.cos(Math.toRadians(heading));
   }
 
   private double calcYchange(int pixels) {
-    return pixels * Math.sin(heading);
+    return pixels * Math.sin(Math.toRadians(heading));
+  }
+
+  public double[] getNextPos() {
+    return new double[]{x, y};
+  }
+
+  public double getHeading() {
+    return heading;
   }
 }
