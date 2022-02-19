f5311dbc354b0c88722d8782864f2067536ee9fc
Brandon Bae <bkb36@duke.edu>
2022-02-19 03:56:35 -0500

    initial compiler exceptions

--- src/main/java/slogo/CompilerExceptions/NotAValueException.java
+++ src/main/java/slogo/CompilerExceptions/NotAValueException.java
@@ -1,2 +1,13 @@
-package slogo.CompilerExceptions;public class NotAValueException {
+package slogo.CompilerExceptions;
+
+public class NotAValueException extends Exception{
+
+  /**
+   * Create an exception based on an issue in our code.
+   */
+  public NotAValueException () {
+    super();
+  }
+
+
 }
