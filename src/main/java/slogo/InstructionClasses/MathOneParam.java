63c3c2c94ba4ad3be70e03b6da5ff4df24be7969
Brandon Bae <bkb36@duke.edu>
2022-02-19 19:04:11 -0500

    added math one parameter abstract class + random math operation

---
+++ src/main/java/slogo/InstructionClasses/MathOneParam.java
@@ -0,0 +1,31 @@
+package slogo.InstructionClasses.MathOperations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import slogo.InstructionClasses.Instruction;
+
+public abstract class MathOneParam extends Instruction {
+  public static final int MATHONEPARAM_NUM_PARAM = 1;
+  private double myParam = 0;
+
+  public MathOneParam() {
+    super(MATHONEPARAM_NUM_PARAM);
+  }
+
+  @Override
+  public void setParameters(Stack<Instruction> valueStack) {
+    //exception if not enough parameters needed to be implemented
+    myParam = valueStack.pop().returnValue();
+  }
+
+  protected double getParam() {
+    return myParam;
+  }
+
+  @Override
+  public void run() {
+    //Math operations do not directly affect the turtle so empty run method
+    return;
+  }
+}
