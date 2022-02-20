4ed7ef926bf5221e0d06991f82b0824c0d9dad39
Thivya Sivarajah <ts346@duke.edu>
2022-02-19 21:29:46 -0500

    Added view and display for turtle

---
+++ src/main/java/slogo/View/TurtleView.java
@@ -0,0 +1,15 @@
+package slogo.View;
+
+/**
+ * View part of turtle that handles UI elements of the turtle and what the user sees
+ * @author Thivya Sivarajah
+ */
+public interface TurtleView {
+
+    /**
+     * updates turtleView with the new coordinates, heading, and other turtle queries from turtle model
+     * @author Thivya Sivarajah
+     */
+    public void updateTurtleView();
+}
+
