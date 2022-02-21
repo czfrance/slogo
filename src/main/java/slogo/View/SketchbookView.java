11046aca6baaa8f2f4b293dce80d943266f03578
Cynthia France <cf219@duke.edu>
2022-02-21 12:50:30 -0500

    fixed sequentialTransition bug

--- src/main/java/slogo/View/SketchbookView.java
+++ src/main/java/slogo/View/SketchbookView.java
@@ -59,6 +59,47 @@ public class SketchbookView {
     }
   }
 
+    public Animation makeAnimation ()
+      throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+      // create something to follow
+      System.out.println("yay");
+      TurtleModel oldModelState = new TurtleModel(myModel.getNextPos()[0], myModel.getNextPos()[1],
+          myModel.getHeading());
+      //MoveTo move = new MoveTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1]));
+      Optional<Object> o = myModel.runNextInsn();
+      if (o.isPresent()) {
+        return getTransition(o, oldModelState);
+      } else {
+        return doNothing(oldModelState);
+      }
+
+//    PathTransition pt = getPathTransition(o, move);
+//    RotateTransition rt = new RotateTransition(Duration.seconds(3));
+//    rt.setByAngle(-90);
+//    return pt;
+//    PathTransition pt = getPathTransition(o, move);
+//    RotateTransition rt = getRotateTransition(o);
+//    return getTransition(o, move);
+//    Optional<Method> m = getChangeFunction(oldModelState);
+//    if (m.isPresent()) {
+//      Object ret = m.get().invoke(this, o, oldModelState);
+//      t = (Transition) ret;
+//    }
+//    else {
+//      //t = new PathTransition(Duration.seconds(NO_MOVEMENT))
+//      t = new RotateTransition(Duration.seconds(NO_MOVEMENT));
+//    }
+//    return new SequentialTransition(turtle, pt, rt);
+  }
+  private PathTransition doNothing(TurtleModel oldModel) {
+    MoveTo move = new MoveTo(convertX(oldModel.getNextPos()[0]), convertY(oldModel.getNextPos()[1]));
+    Path path = new Path();
+    path.getElements().addAll(move,
+        new LineTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1]-1)),
+        new LineTo(convertX(oldModel.getNextPos()[0]), convertY(oldModel.getNextPos()[1])));
+
+    return new PathTransition(Duration.seconds(NO_MOVEMENT), path, turtle);
+  }
 
   /*
    TODO: CREATE PATH AND ROTATION BASED ON WHAT IS CHANGED (MAKE BOTH AND PASS THE
@@ -111,51 +152,54 @@ public class SketchbookView {
 //    //return new SequentialTransition(turtle, pt, rt);
 //  }
 
-  public Animation makeAnimation ()
-      throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-    // create something to follow
-    //TurtleModel oldModelState = myModel;
-    MoveTo move = new MoveTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1]));
-    Optional<Object> o = myModel.runNextInsn();
-    PathTransition pt = getPathTransition(o, move);
-    RotateTransition rt = new RotateTransition(Duration.seconds(3));
-    rt.setByAngle(-90);
-    return pt;
-
-
-//    Transition t;
-//    Optional<Method> m = getChangeFunction(oldModelState);
-//    if (m.isPresent()) {
-//      Object ret = m.get().invoke(this, o, oldModelState, move);
-//      t = (Transition) ret;
-//    }
-//    else {
-//      //t = new PathTransition(Duration.seconds(NO_MOVEMENT))
-//      t = new RotateTransition(Duration.seconds(NO_MOVEMENT));
-//    }
-//    return new SequentialTransition(turtle, t);
-
-    //RotateTransition rt = getRotateTransition();
-//    MoveTo move = new MoveTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1]));
-//    Path path = new Path();
+  //rotating
+//  public Animation makeAnimation ()
+//      throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+//    // create something to follow
+//    //TurtleModel oldModelState = myModel;
 //    Optional<Object> o = myModel.runNextInsn();
-//    Duration currAnimDuration;
-//    if (o.isPresent()) {
-//      path.getElements().addAll(move,
-//          new LineTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1])));
-//      // create an animation where the shape follows a path
-//      currAnimDuration = Duration.seconds((int) o.get() / TURTLE_SPEED);
-//    } else {
-//      currAnimDuration = Duration.seconds(NO_MOVEMENT);
+//    int angle = (int) o.get();
+//    RotateTransition rt = new RotateTransition(Duration.seconds(Math.abs(angle) / TURTLE_TURN_SPEED), turtle);
+//    System.out.println(angle);
+//    rt.setByAngle(angle);
 //
-//    }
-//    return new PathTransition(currAnimDuration, path, turtle);
+////    RotateTransition rt = new RotateTransition(Duration.seconds(3), turtle);
+////    System.out.println(angle);
+////    rt.setByAngle(-90);
+//    return rt;
+//  }
 
-//    // create an animation that rotates the shape
+//  public Animation makeAnimation ()
+//      throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+//    // create something to follow
+//    //TurtleModel oldModelState = myModel;
+//    MoveTo move = new MoveTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1]));
+//    Optional<Object> o = myModel.runNextInsn();
+//    PathTransition pt = getPathTransition(o, move);
 //    RotateTransition rt = new RotateTransition(Duration.seconds(3));
 //    rt.setByAngle(-90);
-//    // put them together in order
-    //return new SequentialTransition(turtle, pt, rt);
+//    return pt;
+//  }
+
+  private Transition getTransition(Optional<Object> o, TurtleModel oldModel)
+      throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+//    MoveTo move = new MoveTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1]));
+//    //return getPathTransition(o, move);
+//    Path p = new Path();
+//    p.getElements().addAll(move,
+//        new LineTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1])));
+//    PathTransition pt = new PathTransition(Duration.seconds(NO_MOVEMENT), p, turtle);
+//    RotateTransition rt = new RotateTransition(Duration.seconds(NO_MOVEMENT), turtle);
+//    if (!oldModel.getNextPos().equals(myModel.getNextPos())) {
+//      pt = getPathTransition(o, oldModel);
+//      //pt = getPathTransition(o, move);
+//    }
+//     else if (!(oldModel.getHeading() == myModel.getHeading())) {
+//      rt = getRotateTransition(o);
+//    }
+    PathTransition pt = getPathTransition(o, oldModel);
+    RotateTransition rt = getRotateTransition(o, oldModel);
+    return new SequentialTransition(turtle, pt, rt);
   }
 
   private Optional<Method> getChangeFunction(TurtleModel oldModel)
@@ -169,11 +213,13 @@ public class SketchbookView {
     return Optional.empty();
   }
 
-  private RotateTransition getRotateTransition(Optional<Object> o, TurtleModel oldModel) {
+  private RotateTransition getRotateTransition(Optional<Object> o, TurtleModel oldModel) {//, TurtleModel oldModel) {
     RotateTransition rt = new RotateTransition();
-    if (o.isPresent()) {
-      rt.setDuration(Duration.seconds((int) o.get() / TURTLE_TURN_SPEED));
-      rt.setByAngle(-1*(double)o.get());
+    rt.setNode(turtle);
+    if (o.isPresent() && !(oldModel.getHeading() == myModel.getHeading())) {
+      int angle = (int)o.get();
+      rt.setDuration(Duration.seconds(Math.abs(angle) / TURTLE_TURN_SPEED));
+      rt.setByAngle(angle);
     } else {
       rt.setDuration(Duration.seconds(NO_MOVEMENT));
       rt.setByAngle(0);
@@ -181,21 +227,28 @@ public class SketchbookView {
     return rt;
   }
 
-  private PathTransition getPathTransition(Optional<Object> o, MoveTo move)
+  private PathTransition getPathTransition(Optional<Object> o, TurtleModel oldModel)
       throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-    //MoveTo move = new MoveTo(convertX(oldModel.getNextPos()[0]), convertY(oldModel.getNextPos()[1]));
+    MoveTo move = new MoveTo(convertX(oldModel.getNextPos()[0]), convertY(oldModel.getNextPos()[1]));
     Path path = new Path();
+    path.getElements().addAll(move,
+        new LineTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1])));
     Duration pathAnimDuration;
-    if (o.isPresent()) {
-      path.getElements().addAll(move,
-          new LineTo(convertX(myModel.getNextPos()[0]), convertY(myModel.getNextPos()[1])));
+    System.out.println(o.isPresent());
+    if (o.isPresent() && moved(oldModel.getNextPos())) {
+      System.out.println("yes");
       // create an animation where the shape follows a path
-      pathAnimDuration = Duration.seconds((int) o.get() / TURTLE_SPEED);
+      pathAnimDuration = Duration.seconds(Math.abs((int) o.get()) / TURTLE_SPEED);
+      return new PathTransition(pathAnimDuration, path, turtle);
     } else {
-      pathAnimDuration = Duration.seconds(NO_MOVEMENT);
-
+      return doNothing(oldModel);
+      //pathAnimDuration = Duration.seconds(NO_MOVEMENT);
     }
-    return new PathTransition(pathAnimDuration, path, turtle);
+
+  }
+
+  private boolean moved(double[] nextPos) {
+    return (nextPos[0] != myModel.getNextPos()[0] || nextPos[1] != myModel.getNextPos()[1]);
   }
 
 //  private PathTransition getPathTransition(Optional<Object> o, TurtleModel oldModel)
