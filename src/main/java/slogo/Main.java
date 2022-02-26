3e4428fd15724f90b442916c67b53accd898915e
Thivya Sivarajah <ts346@duke.edu>
2022-02-26 01:26:52 -0500

    adding stuff back on the frontend because I forgot to merge

--- src/main/java/slogo/Main.java
+++ src/main/java/slogo/Main.java
@@ -95,32 +95,32 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        TurtleModel model = new TurtleModel(0, 0, 90);
-//        Console input = new Console("", new Compiler(""));
-      model.addInsn("forward 100");
-      model.addInsn("back 200");
-      model.addInsn("right 45");
-      model.addInsn("left 90");
-      model.addInsn("setHeading 270");
-      model.addInsn("towards -100 0");
-      model.addInsn("setXY -100 0");
+//        TurtleModel model = new TurtleModel(0, 0, 90);
+////        Console input = new Console("", new Compiler(""));
+//      model.addInsn("forward 100");
+//      model.addInsn("back 200");
+//      model.addInsn("right 45");
+//      model.addInsn("left 90");
+//      model.addInsn("setHeading 270");
+//      model.addInsn("towards -100 0");
+//      model.addInsn("setXY -100 0");
+//
+//      SketchbookView view = new SketchbookView(model);
+//      stage.setScene(view.makeScene());
+//      stage.show();
+//
+//      view.play();
+//      model.addInsn("towards 0 0");
+//      model.addInsn("setXY 0 0");
+//      model.addInsn("towards 0 0");
 
-      SketchbookView view = new SketchbookView(model);
-      stage.setScene(view.makeScene());
-      stage.show();
-
-      view.play();
-      model.addInsn("towards 0 0");
-      model.addInsn("setXY 0 0");
-      model.addInsn("towards 0 0");
-
-//        // let's make someting called dashboard view as a class
-//        SlogoView view = new SlogoView(LANGUAGE);
-//        // give the window a title
-//        stage.setTitle(TITLE);
-//        // add our user interface components to Frame and show it
-//        stage.setScene(view.makeScene(DEFAULT_SIZE.width, DEFAULT_SIZE.height));
-//        stage.show();
+        // let's make someting called dashboard view as a class
+        SlogoView view = new SlogoView(LANGUAGE);
+        // give the window a title
+        stage.setTitle(TITLE);
+        // add our user interface components to Frame and show it
+        stage.setScene(view.makeScene(DEFAULT_SIZE.width, DEFAULT_SIZE.height));
+        stage.show();
 
     }
 
