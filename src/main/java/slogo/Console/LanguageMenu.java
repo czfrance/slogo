fe197b909d7a5fda7914e4ee7b027ba056cad17b
Prajwal Jagadish <Prajwal.Jagadish@duke.edu>
2022-03-04 09:52:37 -0500

    added universal language support still not completely working but very close

--- src/main/java/slogo/Console/LanguageMenu.java
+++ src/main/java/slogo/Console/LanguageMenu.java
@@ -1,9 +1,13 @@
 package slogo.Console;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap.KeySetView;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -12,18 +16,21 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class LanguageMenu {
 
   private final String RESOURCE_PATH = "slogo/languages/";
-  ResourceBundle applicationBundle;
+  private final String PYTHON_SCRIPT = "\\translator\\translateJavaClass.py";
+  ResourceBundle applicationBundle = null;
   ResourceBundle languages;
-  private BorderPane pane = new BorderPane();
+  private VBox pane = new VBox();
   private static final String TITLE = "Choose Your Language";
   private Stage myStage;
   private ComboBox chooseLanguage;
@@ -31,85 +38,86 @@ public class LanguageMenu {
   public LanguageMenu() {
     myStage = new Stage();
     languages = ResourceBundle.getBundle(RESOURCE_PATH + "FixedLanguages");
-    String[] arr = (String[]) languages.keySet().toArray();
     ArrayList<String> combined = new ArrayList<>();
 
     for (String str : languages.keySet())
-      combined.add(languages.getString(str));
+      combined.add(str);
     combined.add("Choose Different Language");
 
     Label l1 = new Label("Choose a Language:");
 
     chooseLanguage = new ComboBox(FXCollections.observableArrayList(combined.toArray()));
-    pane.setAlignment(Pos.CENTER);
-
+    pane.getChildren().addAll(l1, chooseLanguage);
     addListener();
 
-    menu = new Scene(pane, 200, 275);
+    Scene menu = new Scene(pane, 200, 275);
+    myStage.setScene(menu);
     myStage.showAndWait();
   }
 
   private void addListener() {
-    chooseLanguage.accessibleTextProperty().addListener(new ChangeListener<String>() {
+    chooseLanguage.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
       @Override
       public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
         String selectedLanguage = (String) chooseLanguage.getValue();
         if (selectedLanguage != null) {
-          if (selectedLanguage.equals("Choose Different Language")) {
-            selectedLanguage = null;
-            selectedLanguage = newLanguagePrompt();
+          if (selectedLanguage.equals("Choose Different Language"))
+            newLanguagePrompt();
+          else
+            applicationBundle = ResourceBundle.getBundle( RESOURCE_PATH + selectedLanguage);
           }
-          applicationBundle = ResourceBundle.getBundle(
-              RESOURCE_PATH + languages.getString(selectedLanguage));
+        if(applicationBundle != null)
           myStage.close();
         }
-      }
-    });
+      });
   }
+
 
   public ResourceBundle getLanguageBundle() {
     return applicationBundle;
   }
 
-  private String newLanguagePrompt() {
+  private void newLanguagePrompt() {
     Stage s = new Stage();
     s.setTitle("As");
-    TilePane r = new TilePane();
-
+    VBox box = new VBox();
     // create a text input dialog
-    TextInputDialog td = new TextInputDialog("Choose a Real Language");
-
-    // setHeaderText
-    td.setHeaderText("Enter a language");
-
-    // create a button
+    TextField td = new TextField("Choose Real Language");
     Button d = new Button("click");
     d.setOnAction(e -> {
-      try {
-        makeResourcePack(td.getEditor().getText());
-      }catch (Exception e) {
-
-    })
-      r.getChildren().add(d);
-    Scene sc = new Scene(r, 500, 300);
+      if(makeResourcePack(td.getText()));
+        s.close();
+    });
+    box.getChildren().addAll(td,d);
+    Scene sc = new Scene(box, 500, 300);
     s.setScene(sc);
     s.showAndWait();
-    ;
   }
-}
 
 
-  private void makeResourcePack(String language){
+
+  private boolean makeResourcePack(String language) {
     ResourceBundle englishBundle  = ResourceBundle.getBundle(RESOURCE_PATH + "English");
-    Set<String> instructions = (Set<String>) englishBundle.getKeys();
+    String currentWorkingDir = System.getProperty("user.dir");
+    String finalPath = currentWorkingDir + PYTHON_SCRIPT;
+    System.out.println(currentWorkingDir);
+    System.out.println(finalPath);
 
-    for(String str: instructions){
-
+    for(String str: englishBundle.keySet()){
+      System.out.println(str);
       try{
-        ResourceBundle testbundle = ResourceBundle.getBundle(RESOURCE_PATH + language);
+        ProcessBuilder builder = new ProcessBuilder("python " +finalPath +" " +str+" "+language);
+        Process process = builder.start();
+        try{
+          ResourceBundle testbundle = ResourceBundle.getBundle(RESOURCE_PATH + language);
+        }catch (Exception e){
+          return false;
+        }
       }catch (Exception e){
-        throw new NullPointerException();
+        return false;
       }
     }
+    applicationBundle = ResourceBundle.getBundle(RESOURCE_PATH + language);
+    return true;
   }
 }
