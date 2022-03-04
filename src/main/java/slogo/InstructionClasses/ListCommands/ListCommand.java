778811a052c821b131f7722cc864c288e6db8818
Brandon Bae <bkb36@duke.edu>
2022-03-04 07:44:36 -0500

    added all list instructions

--- src/main/java/slogo/InstructionClasses/ListCommands/ListCommand.java
+++ src/main/java/slogo/InstructionClasses/ListCommands/ListCommand.java
@@ -1,2 +1,29 @@
-package slogo.InstructionClasses.ListCommands;public class ListCommand {
+package slogo.InstructionClasses.ListCommands;
+
+import java.util.Stack;
+import java.util.function.BiFunction;
+import slogo.InstructionClasses.Constant;
+import slogo.InstructionClasses.Instruction;
+import slogo.Model.TurtleCollection;
+import slogo.Model.TurtleRecord;
+
+public abstract class ListCommand extends Instruction {
+
+  public ListCommand(int numParam, TurtleCollection collection) {
+    super(numParam, collection);
+  }
+  @Override
+  public abstract double returnValue();
+
+  @Override
+  public BiFunction<Instruction[], TurtleRecord, TurtleRecord> getLambda() {
+    return (Instruction[] myParam, TurtleRecord myRecord) -> myRecord;
+  }
+
+  protected void setVarVal(Instruction variable, double val) {
+    Stack valStack = new Stack<Instruction>();
+    Constant valConstant = new Constant(Double.toString(val));
+    valStack.push(valConstant);
+    variable.setParameters(valStack);
+  }
 }
--- src/main/java/slogo/InstructionClasses/ListCommands/Repeat.java
+++ src/main/java/slogo/InstructionClasses/ListCommands/Repeat.java
@@ -1,2 +1,57 @@
-package slogo.InstructionClasses.ListCommands;public class Repeat {
+package slogo.InstructionClasses.ListCommands;
+
+import java.util.Stack;
+import java.util.function.BiFunction;
+import slogo.InstructionClasses.Constant;
+import slogo.InstructionClasses.InsnList;
+import slogo.InstructionClasses.Instruction;
+import slogo.Model.TurtleCollection;
+import slogo.Model.TurtleRecord;
+
+public class Repeat extends ListCommand {
+
+  public static final int REPEAT_NUM_PARAMS = 2;
+
+  private double numIterations = 0;
+
+  private int currIteration = 0;
+
+  private Instruction myInsnList;
+
+  public Repeat(TurtleCollection model) {
+    super(REPEAT_NUM_PARAMS, model);
+  }
+
+  @Override
+  public void setParameters(Stack<Instruction> valueStack) {
+    Instruction insnList = valueStack.pop();
+    myInsnList = insnList;
+    Instruction expr = valueStack.pop();
+    numIterations = expr.returnValue();
+    valueStack.push(this);
+  }
+
+  @Override
+  public void run() {
+    if(myInsnList.getIsDone()) {
+      currIteration++;
+      myInsnList = new InsnList(myInsnList);
+    }
+    if(currIteration<=numIterations) {
+      myInsnList.run();
+    }
+    else{
+      setIsDone(true);
+    }
+  }
+
+  @Override
+  public double frontEndReturnValue() {
+    return myInsnList.frontEndReturnValue();
+  }
+
+  @Override
+  public double returnValue() {
+    return myInsnList.returnValue();
+  }
 }
