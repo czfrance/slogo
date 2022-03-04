a759d6790b4503cc2ee64370ad12e825cdf50d31
Cynthia France <cf219@duke.edu>
2022-03-04 02:30:15 -0500

    added turtle status function classes

---
+++ src/main/java/slogo/InstructionClasses/TurtleCommands/ShowTurtle.java
@@ -0,0 +1,30 @@
+package slogo.InstructionClasses.TurtleCommands;
+
+import java.util.function.BiFunction;
+import slogo.InstructionClasses.Instruction;
+import slogo.Model.TurtleCollection;
+import slogo.Model.TurtleRecord;
+
+public class ShowTurtle extends TurtleCommand{
+  public static final int SHOW_TURTLE_PARAM_NUM = 0;
+  public static final int SHOWING = 1;
+
+  public ShowTurtle(TurtleCollection turtleModel) {
+    super(SHOW_TURTLE_PARAM_NUM, turtleModel);
+  }
+
+  @Override
+  public BiFunction<Instruction[], TurtleRecord, TurtleRecord> getLambda() {
+    return (Instruction[] params, TurtleRecord myRecord) -> {
+      System.out.println("showTurtle");
+      boolean isShowing = true;
+
+      return new TurtleRecord(myRecord.myX(), myRecord.myY(), myRecord.myHeading(), myRecord.isPenDown(), isShowing);
+    };
+  }
+
+  @Override
+  public double returnValue() {
+    return SHOWING;
+  }
+}
