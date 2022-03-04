5b1db2e4b1e7bf56bdaeaa17b9f969e4d45aa6fb
Prajwal Jagadish <Prajwal.Jagadish@duke.edu>
2022-03-04 17:09:11 -0500

    refactored stuff

--- translator/translate.py
+++ translator/translate.py
@@ -12,26 +12,36 @@ def translatePhrase(phrase, language):
   if(language == "Chinese"):
     id = googletrans.LANGCODES.get("chinese (simplified)")
 
+  if(id == None):
+    return None
+
   translateObject = myTranslator.translate(phrase, src = "en", dest = id)
+
+
 
   if(translateObject.pronunciation != None):
     ret =  translateObject.pronunciation.encode().decode('unicode-escape')
   else:
     ret = translateObject.text.encode().decode('unicode-escape')
-  strencode = ret.encode('ascii', 'ignore')
-  y = strencode.decode()
-  return y
+  #strencode = ret.encode('ascii', 'ignore')
+  #y = strencode.decode()
+  return ret
 
 def main(phrase, language):
   myOSPath = os.getcwd()
   myPathToFile = myOSPath[0:len(myOSPath)-10] + "src/main/resources/slogo/languages/" + language + ".properties"
-  print(myPathToFile)
-  languageFile = open(myPathToFile, "a+")
   translatedText = translatePhrase(phrase, language)
-  languageFile.write(phrase + "=" + translatedText+ "\n")
+
+  if(translatedText == None):
+    return
+
+  languageFile = open(myPathToFile, "a", encoding="utf_8")
+  str = phrase + "=" + translatedText+ "\n"
+  languageFile.write(str)
   print(phrase + "=" + translatedText+ "\n")
   languageFile.close()
 
+  return
 
 if __name__ == '__main__':
   import sys
