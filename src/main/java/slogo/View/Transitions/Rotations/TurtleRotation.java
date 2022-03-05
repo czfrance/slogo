e9bafc859bc9020daccbba13cfa3708be77dc159
Cynthia France <cf219@duke.edu>
2022-03-05 02:36:47 -0500

    refactored turtle's RotateTransition

--- src/main/java/slogo/View/Transitions/Rotations/TurtleRotation.java
+++ src/main/java/slogo/View/Transitions/Rotations/TurtleRotation.java
@@ -1,5 +1,27 @@
 package slogo.View.Transitions.Rotations;
 
+import java.util.Optional;
+import javafx.animation.RotateTransition;
+import slogo.Model.TurtleModel;
+import slogo.View.TurtleView;
+
 public class TurtleRotation {
 
+  RotateTransition turtleRotation;
+
+  public TurtleRotation(Optional<Object> o, TurtleModel oldModel, TurtleModel myCurrModel, TurtleView myView) {
+    turtleRotation = makeRotateTransition(o, oldModel, myCurrModel, myView);
+  }
+
+  public RotateTransition getTurtleRotation() {
+    return turtleRotation;
+  }
+
+  private RotateTransition makeRotateTransition(Optional<Object> o, TurtleModel oldModel, TurtleModel myCurrModel, TurtleView myView) {
+    if (o.isPresent() && !(oldModel.getHeading() == myCurrModel.getHeading())) {
+      return new MovementRotate(o, oldModel, myCurrModel, myView).getRotate();
+    } else {
+      return new DoNothingRotate(o, oldModel, myCurrModel, myView).getRotate();
+    }
+  }
 }
