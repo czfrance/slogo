832e722836e43af7a39e3fca0afa662ca8ffee40
Cynthia France <cf219@duke.edu>
2022-03-05 02:10:01 -0500

    general Transition refactoring

--- src/main/java/slogo/View/Transitions/TurtleTransitions.java
+++ src/main/java/slogo/View/Transitions/TurtleTransitions.java
@@ -1,2 +1,16 @@
-package slogo.View.Transitions;public class TurtleTransitions {
+package slogo.View.Transitions;
+
+import javafx.animation.SequentialTransition;
+import javafx.animation.Transition;
+
+public class TurtleTransitions {
+  SequentialTransition turtleTransition;
+
+  public TurtleTransitions() {
+    turtleTransition = new SequentialTransition();
+  }
+
+  public void addTransition(Transition t) {
+    turtleTransition.getChildren().add(t);
+  }
 }
