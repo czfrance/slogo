2985f4249d43f23e84fd89372e80dd734c9e8094
Cynthia France <cf219@duke.edu>
2022-03-05 02:09:16 -0500

    Path abstract class

--- src/main/java/slogo/View/Transitions/Paths/Path.java
+++ src/main/java/slogo/View/Transitions/Paths/Path.java
@@ -1,2 +1,32 @@
-package slogo.View.Transitions.Paths;public class Path {
+package slogo.View.Transitions.Paths;
+
+import java.awt.Dimension;
+import java.util.Optional;
+import javafx.animation.PathTransition;
+import javafx.scene.Group;
+import slogo.Model.TurtleModel;
+import slogo.View.TurtleView;
+
+public abstract class Path {
+  public static final Dimension DEFAULT_SIZE = new Dimension(400, 400);
+
+  PathTransition path;
+  public Path(Optional<Object> o, TurtleModel oldModel, TurtleModel myCurrModel, TurtleView myView, Group root) {
+    path = makePathTransition(o, oldModel, myCurrModel, myView, root);
+  }
+
+  public PathTransition getPath() {
+    return path;
+  }
+
+  protected abstract PathTransition makePathTransition(Optional<Object> o, TurtleModel oldModel,
+      TurtleModel myCurrModel, TurtleView myView, Group root);
+
+  protected double convertX(double modelX) {
+    return modelX + (DEFAULT_SIZE.width / 2);
+  }
+
+  protected double convertY(double modelY) {
+    return (DEFAULT_SIZE.height / 2) - modelY;
+  }
 }
