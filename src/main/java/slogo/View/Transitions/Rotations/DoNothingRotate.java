9db07138059baf93ad52da8b7598f982906240d0
Cynthia France <cf219@duke.edu>
2022-03-05 02:36:12 -0500

    refactor out two rotateTransition types

--- src/main/java/slogo/View/Transitions/Rotations/DoNothingRotate.java
+++ src/main/java/slogo/View/Transitions/Rotations/DoNothingRotate.java
@@ -1,5 +1,25 @@
 package slogo.View.Transitions.Rotations;
 
+import java.util.Optional;
+import javafx.animation.RotateTransition;
+import javafx.util.Duration;
+import slogo.Model.TurtleModel;
+import slogo.View.TurtleView;
+
 public class DoNothingRotate extends Rotate {
 
+  public DoNothingRotate(Optional<Object> o, TurtleModel oldModel,
+      TurtleModel myCurrModel, TurtleView myView) {
+    super(o, oldModel, myCurrModel, myView);
+  }
+
+  @Override
+  protected RotateTransition makeRotateTransition(Optional<Object> o, TurtleModel oldModel,
+      TurtleModel myCurrModel, TurtleView myView) {
+    RotateTransition rt = new RotateTransition();
+    rt.setNode(myView);
+    rt.setDuration(Duration.seconds(NO_MOVEMENT));
+    rt.setByAngle(0);
+    return rt;
+  }
 }
